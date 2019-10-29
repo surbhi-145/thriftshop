93eef8dc100a5a0e236916bd14b038c8af55a151
surbhi-145 <46847244+surbhi-145@users.noreply.github.com>
2019-10-29 21:11:35 +0530

    Update shop.c

--- shop.c
+++ shop.c
@@ -6,12 +6,13 @@
  **/
 
 #include<stdio.h>
+#include<string.h>
 
 //declaration of product structure
 typedef struct 
 {
     char category[20];
-    char ide_code[5];
+    int ide_code;
     char brand[20];
     char prod_name[40];
     float price;
@@ -24,7 +25,7 @@ typedef struct
 
 typedef struct 
 {
-    char ide_code[5];
+    int ide_code;
     int quantity;
     char review[50];
     int rating;
@@ -33,7 +34,7 @@ typedef struct
 //declaration of user structure
 typedef struct
 {
-    int id;
+    char id[20];
     char password[20];
     char name[40];
     char address[100];
@@ -43,29 +44,90 @@ typedef struct
 void main()
 {
     // Initialization of products database
-    PRODUCT prod_list[20]={{"food","F1","cadbury","oreo",20,"vanilla",5,"mani&sons",{0,0,0},"20g pack"},
-    {"food","F2","quaker","almonds",300,"natural",23,"mani&sons",{0,0,0},"250g"},
-    {"food","F3","quaker","oats",150,"vanilla",10,"mani&sons",{0,0,0},"250g"},
-    {"food","F4","hersheys","nutella",300,"chocolate",5,"mani&sons",{0,0,0},"250g"},
-    {"mobiles","M1","apple","iphone 11 pro",90000,"blue",11,"mango",{0,0,0},1},
-    {"mobiles","M2","google","pixel 4",90000,"not all white",4,"nopixel",{0,0,0},1},
-    {"mobiles","M3","one plus","one plus",50000,"black",50,"oneplusindia",{0,0,0},1},
-    {"mobiles","M4","samsug","samsung galaxy s10",80000,"black",30,"cloudtail",{0,0,0},1},
-    {"books","B1","bloomsbury","the anarchy",250,"softbound",10,"cloudtail",{0,0,0},1},
-    {"books","B2","bloomsburry","the institute",700,"hardbound",3,"anush",{0,0,0},1},
-    {"books","B3","harvil secker","homo deus",300,"softbound",15,"kobo",{0,0,0},1},
-    {"books","B4","random house","norweign wood",150,"softbound",10,"lolo",{0,0,0},1},
-    {"men's fashion","MF1","diesel","tshirt",1000,"black",7,"shopclues",{0,0,0},1},
-    {"men's fashion","MF2","diesel","shirt",2000,"blue",13,"shopnoclues",{0,0,0},1},
-    {"men's fashion","MF3","levis","jeans",2000,"white",2,"shopclues",{0,0,0},1},
-    {"men's fashion","MF4","adidas","shoes",5000,"yellow",20,"dcathlon",{0,0,0},1},
-    {"women's fashion","WF1","hnm","dress",3000,"black",7,"myspace",{0,0,0},1},
-    {"women's fashion","WF2","hnm","tshirt",1500,"red",5,"seezz",{0,0,0},1},
-    {"women's fashion","WF3","levis","jeans",2500,"blue",4,"myntra",{0,0,0},1},
-    {"women's fashion","WF4","forever 21","jacket",3000,"yellow",10,"F21",{0,0,0},1},
+    PRODUCT prod_list[20]={{"food","01","cadbury","oreo",20,"vanilla",5,"mani&sons",{0,0,0},"20g pack"},
+    {"food","02","quaker","almonds",300,"natural",23,"mani&sons",{0,0,0},"250g"},
+    {"food","03","quaker","oats",150,"vanilla",10,"mani&sons",{0,0,0},"250g"},
+    {"food","04","hersheys","nutella",300,"chocolate",5,"mani&sons",{0,0,0},"250g"},
+    {"mobiles","11","apple","iphone 11 pro",90000,"blue",11,"mango",{0,0,0},1},
+    {"mobiles","12","google","pixel 4",90000,"not all white",4,"nopixel",{0,0,0},1},
+    {"mobiles","13","one plus","one plus",50000,"black",50,"oneplusindia",{0,0,0},1},
+    {"mobiles","14","samsug","samsung galaxy s10",80000,"black",30,"cloudtail",{0,0,0},1},
+    {"books","21","bloomsbury","the anarchy",250,"softbound",10,"cloudtail",{0,0,0},1},
+    {"books","22","bloomsburry","the institute",700,"hardbound",3,"anush",{0,0,0},1},
+    {"books","23","harvil secker","homo deus",300,"softbound",15,"kobo",{0,0,0},1},
+    {"books","24","random house","norweign wood",150,"softbound",10,"lolo",{0,0,0},1},
+    {"men's fashion","31","diesel","tshirt",1000,"black",7,"shopclues",{0,0,0},1},
+    {"men's fashion","32","diesel","shirt",2000,"blue",13,"shopnoclues",{0,0,0},1},
+    {"men's fashion","33","levis","jeans",2000,"white",2,"shopclues",{0,0,0},1},
+    {"men's fashion","34","adidas","shoes",5000,"yellow",20,"dcathlon",{0,0,0},1},
+    {"women's fashion","41","hnm","dress",3000,"black",7,"myspace",{0,0,0},1},
+    {"women's fashion","42","hnm","tshirt",1500,"red",5,"seezz",{0,0,0},1},
+    {"women's fashion","43","levis","jeans",2500,"blue",4,"myntra",{0,0,0},1},
+    {"women's fashion","44","forever 21","jacket",3000,"yellow",10,"F21",{0,0,0},1}
     };
 
+    //Initialization of user database 
+    USER users[6]={{"surbhi_145","123new","surbhi agrawal","BPCL colony , Mumbai",{{41,1,"excellent",5}}},
+    {"saumballs","66669999","saumya gandhi","SMGSK, Gurgaon",{{23,1,"excellent",4}}},
+    {"zac","toocoolboy","zac efron","PTSD, Mumbai",{{23,1,"excellent",4},{32,1,"okayishh",3}}},
+    {"shahkhan","yesno00","shah rukh khan","Mannat, Mumbai",{{21,2,"excellent",5}}},
+    {"alia","okaybye","alia bhatt","221B, Baker's Street, Gurgaon",{{11,1,"the best!",5},{02,1,"good",3}}}
+    };
 
+    //Homepage
+    int user_length=5;
+    int login=0; //flag for successful login 
+    printf("Hello, Welcome to the thrift shop. please select an option from the given menu to continue.\n1. Login\n 2. Sign Up\n");
+    int option;
+    scanf("%d",&option);
+
+    //sign up 
+    if(option==2)
+    {   
+        user_length++;
+        printf("Please Enter your Full Name\n");
+        scanf("%s",&users[6].name);
+        printf("Please Enter a valid Username\n");
+        scanf("%s",&users[6].id);
+        printf("Please Enter a valid Password\n");
+        scanf("%s",&users[6].password);
+        printf("Please Enter your Address\n");
+        scanf("%s",&users[6].address);
+
+        printf("Please select a valid option to continue \n 1. continue with this account 2. sign in to another account");
+        int option2;
+        scanf("%d",&option2);
+
+        if(option ==1)
+        login =1;
+    }
+
+    while(login==0)
+    {   
+        char id[20];
+        char password[20];
+        printf("Please Enter your user id\n");
+        scanf("%s",&id);
+        printf("Please Enter your user password\n");
+        scanf("%s",&password);
+        for(int i=0; i<user_length && login==0; i++)
+        {
+            if(strcmp(id,users[i].id)==0)
+            {
+                if(strcmp(password,users[i].password)==0)
+                {
+                    login==1;
+                    printf("Congratulations! You have successfully logged in!\n");
+                }
+            }
+        }
+        
+        if(login==0)
+        printf("The User name or password that you entered is incorrect. Please try again.\n");
+
+    }
+
+    
 }
 
 
